a2d0c2e056cbe1e2cbe61a369f68bb543125c210
Aleks Budzko <alexrfictcb@gmail.com>
2025-02-15 22:07:56 +0300

    .

--- src/main/java/com/bookstorage/BookStorageServiceApplication.java
+++ src/main/java/com/bookstorage/BookStorageServiceApplication.java
@@ -1,11 +1,12 @@
-package com.book.storageservice;
+package com.bookstorage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.bookstorage"})
 public class BookStorageServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStorageServiceApplication.class, args);
 	}
 }
+
